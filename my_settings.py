c6571be96304e0e5108e039a20b0314f07725b13
유민혁 <93bulldozer@yuminhyeog-ui-MacBookAir.local>
2021-11-15 20:22:12 +0900

    first commit

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks',
+        'USER': 'root',
+        'PASSWORD': '19930905',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+		'OPTIONS': {'charset': 'utf8mb4'}
+    }
+}
+
+SECRET_KEY = 'django-insecure-p27vq!buasj4-gjt7#+s*ylj@$19csvr6(m80x*n#hpt)hy%&%'
+
